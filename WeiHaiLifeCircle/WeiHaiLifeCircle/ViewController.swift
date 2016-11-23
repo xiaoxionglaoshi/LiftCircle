f3c8776f2be0fda356a804cec084bf32ec24f520
xiaoxionglaoshi <wjncode@gmail.com>
2016-11-23 11:07:35 +0800

    initial

---
+++ WeiHaiLifeCircle/WeiHaiLifeCircle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeiHaiLifeCircle
+//
+//  Created by mainone on 16/11/23.
+//  Copyright © 2016年 wjn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
